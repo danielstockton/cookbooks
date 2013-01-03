dd4579cb1e7dde95a9826dfa5ea6055b12f7fbd8
Daniel Stockton <me@danielstockton.com>
2013-01-04 00:20:56 +0200

    Change deployer to group_name sudo instead of gid

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,7 +4,7 @@ package "vim"
 
 user node[:user][:name] do
   password node[:user][:password]
-  gid "sudo"
+  group_name "sudo"
   home "/home/#{node[:user][:name]}"
   supports manage_home: true
   shell "/bin/zsh"
