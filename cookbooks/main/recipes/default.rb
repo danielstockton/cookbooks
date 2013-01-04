3e5b32c51a8dd8c1b135a796613e93b986c9942c
Daniel Stockton <me@danielstockton.com>
2013-01-04 14:45:37 +0200

    Change group_name back to gid as group_name is unrecognised

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,7 +4,7 @@ package "vim"
 
 user node[:user][:name] do
   password node[:user][:password]
-  group_name "sudo"
+  gid "sudo"
   home "/home/#{node[:user][:name]}"
   supports manage_home: true
   shell "/bin/zsh"
